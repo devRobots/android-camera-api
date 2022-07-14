027db6850db0dbe02908654425662068f9420d8a
Yesid Rosas Toro <ysrosast@gmail.com>
2022-07-13 20:46:55 -0500

    Correccion de ruta de API

--- src/routes/images.routes.js
+++ src/routes/images.routes.js
@@ -3,7 +3,7 @@ import { imageController } from "../controllers/images.controller.js";
 
 const router = Router();
 
-router.get("/images", imageController.getImages);
-router.post("/images", imageController.postImage);
+router.get("/api/images", imageController.getImages);
+router.post("/api/images", imageController.postImage);
 
 export default router;
